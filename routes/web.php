446af2185cd6b918353838d725a713372d86fc09
Naif Mohamed <naifmhd@gmail.com>
2026-02-19 13:31:05 +0500

    wip

--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,12 @@ use Inertia\Inertia;
 use Laravel\Fortify\Features;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canRegister' => Features::enabled(Features::registration()),
-    ]);
+    return redirect()->route('voters.index');
 })->name('home');
 
 Route::get('dashboard', function () {
-    return Inertia::render('Dashboard');
+    // return Inertia::render('Dashboard');
+    return redirect()->route('voters.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('voters', [VoterController::class, 'index'])
